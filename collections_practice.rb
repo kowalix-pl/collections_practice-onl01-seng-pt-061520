2deb2637aeda4871fe2af404d5a67bd0bfbbf238
kowalix007 L.K <kowalix007@gmail.com>
2020-06-21 09:16:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+ array.sort {|a,b| a <=> b}
+end
 
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+   array.sort {|a,b| a.length <=> b.length}
+end
+
+# def swap_elements(array)
+#   array.sort {|a,b| b <=> a}
+# end
+
+def reverse_array(array)
+ array.reverse
+end 
+
+def kesha_maker(array) 
+ kesha_array = []
+ kesha_array << array.each {|word| 
+ kesha_array
+end
