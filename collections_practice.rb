18e6058fffa055907867ed7952eaf8c489411526
kowalix007 L.K <kowalix007@gmail.com>
2020-06-21 09:42:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,10 +11,9 @@ def sort_array_char_count (array)
    array.sort {|a,b| a.length <=> b.length}
 end
 
-# def swap_elements(array)
-#   array.sort {|a,b| b <=> a}
-
-# end
+def swap_elements(array,index)
+  array.each {|a,b| a[1] }
+ end
 
 def reverse_array (array)
  array.reverse
