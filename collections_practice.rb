e71b3d1aeac432869e33abff46480713c30850e2
kowalix007 L.K <kowalix007@gmail.com>
2020-06-21 09:30:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,6 +32,6 @@ def sum_array(array)
  array.inject {|sum, number| sum+number}
 end 
 
-# def add_s (array) 
-#   array.each_with_index {|element,index| index != 1 element << "s"}
-# end 
+def add_s (array) 
+  array.each_with_index {|element,index| index != 1 element << "s"}
+end 
